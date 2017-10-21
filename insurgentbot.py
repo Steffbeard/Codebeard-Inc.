4a9f07bb3b97a6b40f2cdee64b925e714e220279
Steffbeard <Steffbeard@users.noreply.github.com>
2017-10-21 18:46:02 -0400

    added bells and whistles

--- insurgentbot.py
+++ insurgentbot.py
@@ -1,8 +1,9 @@
 import discord
 from discord.ext import commands
 import random
+import time
 
-description = '''InsurgentBot coded by Steffbeard. For use in the Insurgency Gym.
+description = '''InsurgentBot coded by Steffbeard and FreeDoum. For use in the Insurgency Gym.
 There are a number of utility commands being showcased here.'''
 bot = commands.Bot(command_prefix='.', description=description)
 
@@ -25,6 +26,7 @@ async def on_ready():
     print("-----------------------------")
 
 @bot.command()
+@commands.cooldown(1, .5, commands.BucketType.user)
 async def add(ctx, left: int, right: int):
     """Adds two numbers together."""
     await ctx.send(left + right)
@@ -42,6 +44,7 @@ async def roll(ctx, dice: str):
     await ctx.send(result)
 
 @bot.command()
+@commands.cooldown(1, .5, commands.BucketType.user)
 async def startvote(ctx):
     """Starts a vote for the map"""
     user = ctx.message.author
@@ -59,6 +62,7 @@ async def startvote(ctx):
         await ctx.send('```Please vote using .vote followed by the number associated with the map you desire the most\n\n1: District\n2: Embassy\n3: Market\n4: Uprising\n5: Station\n6: Verticality\n7: Heights\n8: Siege\n9: Ministry```')
 
 @bot.command()
+@commands.cooldown(1, .5, commands.BucketType.user)
 async def vote(ctx, mapNum: int):
     """Registers vote"""
     user = ctx.message.author
@@ -83,6 +87,7 @@ async def vote(ctx, mapNum: int):
         await ctx.send('Vote for {} registered'.format(mapName))
 
 @bot.command()
+@commands.cooldown(1, .5, commands.BucketType.user)
 async def endvote(ctx):
     """Terminates the vote for the map"""
     user = ctx.message.author
@@ -107,11 +112,13 @@ async def endvote(ctx):
     vote = False
 
 @bot.command(description='For when you wanna settle the score some other way')
+@commands.cooldown(1, .5, commands.BucketType.user)
 async def choose(ctx, *choices: str):
     """Chooses between multiple choices."""
     await ctx.send(random.choice(choices))
 
 @bot.group()
+@commands.cooldown(1, .5, commands.BucketType.user)
 async def cool(ctx):
     """Says if a user is cool.
     In reality this just checks if a subcommand is being invoked.
@@ -120,18 +127,21 @@ async def cool(ctx):
         await ctx.send('No, {0.subcommand_passed} is not cool, only FreeDoum is.'.format(ctx))
 
 @cool.command(name='bot')
+@commands.cooldown(1, .5, commands.BucketType.user)
 async def _bot(ctx):
     """Is the bot cool?"""
     await ctx.send('Yes, the bot is cool.')
 
 @bot.command()
+@commands.cooldown(1, .5, commands.BucketType.user)
 async def pickmap(ctx):
     """Picks a random map from the map pool"""
     await ctx.send(random.choice(mapPool))
 
 @bot.command()
+@commands.cooldown(1, .5, commands.BucketType.user)
 async def callout(ctx, map):
-    """Gives you a video to the callouts for the specific map. Use .callout (map name)"""
+    """Gives you a video to the callouts for the specific map."""
     map = map.lower()
     if map == "district":
         await ctx.send("Here you go: https://www.youtube.com/watch?v=LIUJM8guWrE&list=PL7a9PMAEeinpjMD0UXdlUy5hywhzHHjr8&index=4")
@@ -152,5 +162,40 @@ async def callout(ctx, map):
     elif map == "verticality":
         await ctx.send("Here: https://www.youtube.com/watch?v=ITIfjU55SkI&index=10&list=PL7a9PMAEeinpjMD0UXdlUy5hywhzHHjr8")
 
+@bot.command()
+async def invite(ctx):
+    """Gives an invite link for the bot."""
+    await ctx.send("https://discordapp.com/oauth2/authorize?client_id=370012655202598912&scope=bot&permissions=1341643969")
+
+@bot.command()
+@commands.cooldown(1, .5, commands.BucketType.user)
+async def info(ctx):
+    """Provides info about the bot."""
+    embed = discord.Embed(color=0x41454E, description="InsurgentBot by Steff and FreeDoum.")
+    embed.title = "Info about InsurgentBot:"
+    embed.add_field(name="What is InsurgentBot?", value="InsurgentBot is a Discord bot written in the discord.py library. It has many fun commands for users.", inline=False)
+    embed.add_field(name="Who made this bot?", value="Coded by Steffbeard and FreeDoum, additional refrence by Mippy.", inline=False)
+    embed.add_field(name="Credits to:", value="[Rapptz/discord.py](https://github.com/Rapptz/discord.py), [The users in the Discord API server](https://discord.gg/discord-api), [The Insurgency Gym](https://discord.gg/658eVDM), [Mippy](https://williamlomas.me)", inline=False)
+    await ctx.send(embed=embed)
+
+@bot.command()
+@commands.cooldown(1, 3, commands.BucketType.user)
+async def stats(ctx):
+    """Shows InsurgentBot's stats."""
+    servercount = len(bot.guilds)
+    usercount = len(set(bot.get_all_members()))
+    channelcount = len(set(bot.get_all_channels()))
+    t1 = time.perf_counter()
+    message = await ctx.send("Checking stats... :cd:")
+    t2 = time.perf_counter()
+    ping = round((t2-t1)*1000)
+    embed = discord.Embed(color=0x41454E, description="Current Bot Stats")
+    embed.title = "InsurgentBot Stats:"
+    embed.add_field(name="Ping", value="{}ms".format(ping))
+    embed.add_field(name="Servers", value=servercount)
+    embed.add_field(name="Users", value=usercount)
+    embed.add_field(name="Channels", value=channelcount)
+    await message.delete()
+    await ctx.send(embed=embed)
 
 bot.run('MzcwMDEyNjU1MjAyNTk4OTEy.DMg5fQ.3Jzs2volpAGyNEwI8TQS-raiKpA')
