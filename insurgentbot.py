14c052ca4a4f561f2846f89be49f3c0b70f853fd
Steffbeard <Steffbeard@users.noreply.github.com>
2017-10-21 12:22:13 -0400

    Update insurgentbot.py

--- insurgentbot.py
+++ insurgentbot.py
@@ -5,14 +5,21 @@ import random
 description = '''InsurgentBot coded by Steffbeard. For use in the Insurgency Gym.
 
 There are a number of utility commands being showcased here.'''
-bot = commands.Bot(command_prefix='+', description=description)
+bot = commands.Bot(command_prefix='.', description=description)
 
 @bot.event
 async def on_ready():
-    print('Logged in as')
-    print(bot.user.name)
-    print(bot.user.id)
-    print('------')
+    users = len(set(bot.get_all_members()))
+    guilds = len(bot.guilds)
+    game = discord.Game(name=".help | Coded by Steffbeard and FreeDoum.")
+    await bot.change_presence(status=discord.Status.online, game=game)
+    print("-----------------------------")
+    print("Martydom is overrated")
+    print("-----------------------------")
+    print("Connected to {} guilds".format(guilds))
+    print("-----------------------------")
+    print("Currently serving {} users".format(users))
+    print("-----------------------------")
 
 @bot.command()
 async def add(ctx, left: int, right: int):
@@ -37,12 +44,6 @@ async def choose(ctx, *choices: str):
     await ctx.send(random.choice(choices))
 
 @bot.command()
-async def repeat(ctx, times: int, content='repeating...'):
-    """Repeats a message multiple times."""
-    for i in range(times):
-        await ctx.send(content)
-
-@bot.command()
 async def joined(ctx, member: discord.Member):
     """Says when a member joined."""
     await ctx.send('{0.name} joined in {0.joined_at}'.format(member))
@@ -53,7 +54,7 @@ async def cool(ctx):
     In reality this just checks if a subcommand is being invoked.
     """
     if ctx.invoked_subcommand is None:
-        await ctx.send('No, {0.subcommand_passed} is not cool'.format(ctx))
+        await ctx.send('No, {0.subcommand_passed} is not cool, only FreeDoum is.'.format(ctx))
 
 @cool.command(name='bot')
 async def _bot(ctx):
@@ -65,4 +66,29 @@ async def pickmap(ctx):
     """Picks a random map from the map pool"""
     await ctx.send(random.choice(["District", "Heights", "Market", "Ministry", "Station", "Siege", "Uprising", "Embassy", "Verticality"]))
 
+@bot.command()
+async def callout(ctx, map):
+    """Gives you a video to the callouts for the specific map. Use .callout (map name)"""
+    map = map.lower()
+    if map == "district":
+        await ctx.send("Here you go: https://www.youtube.com/watch?v=LIUJM8guWrE&list=PL7a9PMAEeinpjMD0UXdlUy5hywhzHHjr8&index=4")
+    elif map == "heights":
+        await ctx.send("Here you are: https://www.youtube.com/watch?v=rH0pOM2w_xo")
+    elif map == "market":
+        await ctx.send("Here: https://www.youtube.com/watch?v=-B8VUUG8XQk&")
+    elif map == "ministry":
+        await ctx.send("Here you go: https://www.youtube.com/watch?v=_HCdrwFo8FU&")
+    elif map == "station":
+        await ctx.send("Here you are: https://www.youtube.com/watch?v=JEF2sG-KRIU")
+    elif map == "siege":
+        await ctx.send("Here: https://www.youtube.com/watch?v=JxmcAIU_lnY&list=PL7a9PMAEeinpjMD0UXdlUy5hywhzHHjr8&index=7")
+    elif map == "uprising":
+        await ctx.send("Here you go: https://www.youtube.com/watch?v=MUyQIhYA7-I")
+    elif map == "embassy":
+        await ctx.send("Here you are: https://www.youtube.com/watch?v=fmT18WHiPYM&index=5&list=PL7a9PMAEeinpjMD0UXdlUy5hywhzHHjr8")
+    elif map == "verticality":
+        await ctx.send("Here: https://www.youtube.com/watch?v=ITIfjU55SkI&index=10&list=PL7a9PMAEeinpjMD0UXdlUy5hywhzHHjr8")
+
+
 bot.run('MzcwMDEyNjU1MjAyNTk4OTEy.DMg5fQ.3Jzs2volpAGyNEwI8TQS-raiKpA')
+
